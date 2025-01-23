5c5ef3417025c094fbe79c8857b3632fea2a41d2
Programer_amatir <khalifnalbanb60@gmail.com>
2025-01-23 18:32:08 +0700

    menambahkan responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
 // Clear form before unload
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName("form")) {
